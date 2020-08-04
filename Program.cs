a0f595cee3d1f8fd09c221e8613bdad1cbd266a8
ekmekarasikodlama <67587068+ekmekarasikodlama@users.noreply.github.com>
2020-08-04 15:29:42 +0300

    Saat Videosu

--- Program.cs
+++ Program.cs
@@ -4,74 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace sifrekontrol
+namespace saatuygulamasıkonsol
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Bir tane sayı
-            //6 karakterden uzun
-            //1 büyük harf olmalı
-            char[] karakterler;
+            //tarih
+            //-kısa tarih
+            //-uzun tarih
+            //saat
+            //-kısa saat
+            //-uzun saat
+            //belirli yıl/ay/gün/saat/dakika sonraki tarih
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine("----");
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+            Console.WriteLine("----");
+            Console.Write("Eklenecek yılı yazın:");
+            string eklenecekyil = Console.ReadLine();
+            Console.Write("Eklenecek ayı yazın:");
+            string eklenecekay = Console.ReadLine();
+            Console.Write("Eklenecek günü yazın:");
+            string eklenecekgun = Console.ReadLine();
+            Console.Write("Eklenecek saat yazın:");
+            string ekleneceksaat = Console.ReadLine();
+            Console.Write("Eklenecek dakika yazın:");
+            string eklenecekdakika = Console.ReadLine();
 
-            baslangic();
+            Console.WriteLine("Ortaya çıkan tarih: " + DateTime.Now.AddYears(int.Parse(eklenecekyil)).AddMonths(int.Parse(eklenecekay)).AddDays(int.Parse(eklenecekgun)).AddHours(int.Parse(ekleneceksaat)).AddMinutes(int.Parse(eklenecekdakika)));
 
-            void baslangic()
-            {
-                Console.Write("Şifre girin:");
-                string girilenSifre = Console.ReadLine();
-                karakterler = girilenSifre.ToArray();
-                sayikontrol();
-            }
-
-            void sayikontrol()
-            {
-                foreach(char karakter in karakterler)
-                {
-                    if(char.IsNumber(karakter))
-                    {
-                        uzunlukKontrol();
-                        return;
-                    }
-                }
-                Console.WriteLine(Environment.NewLine + "En az bir sayı girilmeli." + Environment.NewLine);
-                baslangic();
-            }
-
-            void uzunlukKontrol()
-            {
-                if(karakterler.Length >= 6)
-                {
-                    buyukHarfKontrol();
-                }
-                else
-                {
-                    Console.WriteLine(Environment.NewLine + "En az 6 karakter uzunluğunda olmalı" + Environment.NewLine);
-                    baslangic();
-                }
-
-            }
-
-            void buyukHarfKontrol()
-            {
-                foreach(char karakter in karakterler)
-                {
-                    if(char.IsUpper(karakter))
-                    {
-                        sonlandir();
-                        return;
-                    }
-                }
-                Console.WriteLine(Environment.NewLine + "En az bir büyük harf olmalıdır" + Environment.NewLine);
-                baslangic();
-            }
-
-            void sonlandir()
-            {
-                Console.WriteLine(Environment.NewLine + "Şifre uygun.");
-                Console.ReadLine();
-            }
+            Console.ReadLine();
         }
     }
 }
